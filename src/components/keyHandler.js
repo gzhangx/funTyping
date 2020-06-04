e58d7f5965619f68a9832e6d9f3c61e577c3ceba
Gang Zhang <gzhangx@hotmail.com>
2020-06-04 10:05:22 -0400

    test

--- src/components/keyHandler.js
+++ src/components/keyHandler.js
@@ -1,4 +1,4 @@
-import Reat, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 const KeyHandler = cb => {
     const [keyPressed, setKeyPressed] = useState();
     useEffect(() => {

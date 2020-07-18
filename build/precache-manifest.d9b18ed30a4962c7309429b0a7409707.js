ea0aa17faed745aa9a974fe29864c2ceb174c750
Gang Zhang <gzhangx@hotmail.com>
2020-07-18 18:54:49 -0400

    build

---
+++ build/precache-manifest.d9b18ed30a4962c7309429b0a7409707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42e9409d0ac5b5989658feffb4a4097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42d7925f1f4f41502dfb",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "cd79c57dc3e0ed3e756b",
+    "url": "/static/js/2.1a411945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a411945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d7925f1f4f41502dfb",
+    "url": "/static/js/main.cffa745d.chunk.js"
+  },
+  {
+    "revision": "02cdb78882fea7e37eef",
+    "url": "/static/js/runtime-main.b99ac7c7.js"
+  }
+]);

9aa35aa0ca236d036ede30661a49d93522413b3a
Gang Zhang <gzhangx@hotmail.com>
2020-07-24 22:51:32 -0400

    disable control f

--- src/components/keyHandler.js
+++ src/components/keyHandler.js
@@ -4,6 +4,9 @@ const KeyHandler = cb => {
     useEffect(() => {
       const onKeyDown = e => {
         const { key } = e;
+        if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {         
+          e.preventDefault();
+        }
           if (key === 'Backspace' || key == 'Control') e.preventDefault();
           setKeyPressed(key);
           if (cb) cb(key);
